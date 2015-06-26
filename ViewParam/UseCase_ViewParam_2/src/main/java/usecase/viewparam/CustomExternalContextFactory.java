89f32d5259dc612a602a62012d5d87af8e2f6ee1
AnghelLeonard <leoprivacy@yahoo.com>
2015-06-26 16:19:59 +0300

    Suppressing invocation of the converter regardless the null values

---
+++ ViewParam/UseCase_ViewParam_2/src/main/java/usecase/viewparam/CustomExternalContextFactory.java
@@ -0,0 +1,29 @@
+package usecase.viewparam;
+
+import javax.faces.FacesException;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.ExternalContextFactory;
+
+/**
+ *
+ * @author Anghel Leonard
+ */
+public class CustomExternalContextFactory extends ExternalContextFactory {
+    
+    private ExternalContextFactory externalContextFactory;
+    
+    public CustomExternalContextFactory(){       
+    }
+    
+    public CustomExternalContextFactory(ExternalContextFactory externalContextFactory){
+        this.externalContextFactory = externalContextFactory;
+    }
+
+    @Override
+    public ExternalContext getExternalContext(Object context, Object request, Object response) throws FacesException {
+        
+        ExternalContext handler = new CustomExternalContext(externalContextFactory.getExternalContext(context, request, response));
+        return handler;
+    }
+    
+}
